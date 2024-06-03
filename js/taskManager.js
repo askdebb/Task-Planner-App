95d4671735ec46c432ab3388a3385462ca608541
Caroline <carolking109@gmail.com>
2024-06-03 13:42:42 +0000

    working on task 5

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,4 +1,4 @@
-class TaskManager {
+ class TaskManager {
     constructor(currentId = 0) {
       this.currentId = currentId;
       this.tasks = [];
@@ -23,3 +23,21 @@ class TaskManager {
   task.addTask('Shopping List', 'Create Christmas shopping list', 'Agya Christopher', '6/10/2024', 'To-Do' );
 
   console.log(task.tasks)
+
+
+
+
+//   const newTaskForm = document.querySelector('#new-task-form');
+// newTaskForm.addEventListener('submit', (event) => {
+//   event.preventDefault();
+
+
+//   taskManager.addTask(name, description, assignedTo, dueDate);
+
+//   nameInput.value = '';
+//   descriptionInput.value = '';
+//   assignedToInput.value = '';
+//   dueDateInput.value = '';
+// });
+
+module.exports= {TaskManager}
